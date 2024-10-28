44233aef40a378af86747af32eeedfea8ec77f97
PranjalShinde16 <shinde_p@me.iitr.ac.in>
2024-10-28 23:39:09 +0530

    added three projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,9 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+
+export const ProjectCard = ({ title, description, imgUrl, link }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
@@ -8,6 +11,9 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
+          <br/>
+          <a href={link} style={{textDecoration:"Underline",marginTop:"30px",color:"white"}} target="_blank" rel="noopener noreferrer">Open Link<FontAwesomeIcon icon={faExternalLinkAlt} />
+          </a>
         </div>
       </div>
     </Col>
