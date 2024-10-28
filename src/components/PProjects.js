8001638c80343303a52f1fcb5ea53192e474c23a
PranjalShinde16 <shinde_p@me.iitr.ac.in>
2024-10-28 23:03:00 +0530

    projects setup

--- src/components/PProjects.js
+++ src/components/PProjects.js
@@ -9,7 +9,7 @@ import TrackVisibility from 'react-on-screen';
 
 export const PProjects = () => {
 
-  const projects = [
+  const projects1 = [
     {
       title: "Business Startup",
       description: "Design & Development",
@@ -25,6 +25,27 @@ export const PProjects = () => {
       description: "Design & Development",
       imgUrl: projImg3,
     },
+  ];
+
+  const projects2 = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  const projects3 = [
     {
       title: "Business Startup",
       description: "Design & Development",
@@ -51,7 +72,7 @@ export const PProjects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Presenting a range of projects that transform ideas into functional and engaging solutions</p>
+                <p style={{marginBottom:"40px"}}>Presenting a range of projects that transform ideas into engaging solutions</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
@@ -68,7 +89,7 @@ export const PProjects = () => {
                     <Tab.Pane eventKey="first">
                       <Row>
                         {
-                          projects.map((project, index) => {
+                          projects1.map((project, index) => {
                             return (
                               <ProjectCard
                                 key={index}
@@ -79,12 +100,34 @@ export const PProjects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                      </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
+                    <Row>
+                        {
+                          projects3.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                       </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
               </div>}
